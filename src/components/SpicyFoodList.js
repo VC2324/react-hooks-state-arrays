bf9b11faff31b75cebef01e244fc70421f97c526
Victor <victorac2324@gmail.com>
2024-04-05 11:13:50 -0400

    did one deliverable

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,7 +6,10 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
+
   }
 
   const foodList = foods.map((food) => (
